9820699a3b9bf19835dcc0c3ffc9c2eff3200b27
Nagendra Chinnam <nagendra.chinnam@ct.gov>
2026-03-30 21:30:03 -0400

    New changes

---
+++ C Class/programs/ammu_math.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+// Ammu Math Library - Tool 1
+
+int SumOfTwoProperties(int a, int b)
+{
+    printf("Calculating the sum of a and b is %d\n", a + b);
+
+    return a + b;
+}
+
+
---
+++ C Class/programs/hello_world.c
@@ -0,0 +1,12 @@
+
+#include "ammu_math.h"
+
+int main()
+{
+    
+    int result = SumOfTwoProperties(5, 10);
+
+    printf("The result of the sum is: %d\n", result);
+
+    return 0;
+}
