f04efeb7ff3a72e409a8c2649369962e171c5d2f
Nagendra Chinnam <nagendra.chinnam@ct.gov>
2026-04-08 22:00:52 -0400

    some changes

--- C Class/programs/ammu_math.h
+++ C Class/programs/ammu_math.h
@@ -1,13 +1,20 @@
 
-#include <stdio.h>
-
-// Ammu Math Library - Tool 1
+int generic_result = 0; // global variable to store the result of any operation
 
 int SumOfTwoProperties(int a, int b)
 {
-    printf("Calculating the sum of a and b is %d\n", a + b);
+    int result = a + b;  // local variable to store the result of addition
+    generic_result = result; // store the result in the global variable
+    return result;
+}
 
-    return a + b;
+int SubtractionOfTwoProperties(int a, int b)
+{
+    int result1 = a - b; // local variable to store the result of subtraction
+    
+    generic_result = result1;
+
+    return result1;
 }
 
 
--- C Class/programs/hello_world.c
+++ C Class/programs/hello_world.c
@@ -1,12 +1,16 @@
 
+#include <stdio.h>
 #include "ammu_math.h"
 
-
-int main()   // is the entry point of the program
+int main()  // is the entry point of the program
 {
+
+    int result1 = SumOfTwoProperties(10, 20);
 
     printf("I got 100%% marks in the exam\n");
 
+    result1 = generic_result;
 
     return 0;   // 0 means the program ended successfully
 }
+
