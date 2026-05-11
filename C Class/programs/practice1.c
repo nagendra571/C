01097a7f48ffa335201be0e86b3ce6b0b53caf65
Nagendra Chinnam <nagendra.chinnam@ct.gov>
2026-05-10 22:18:23 -0400

    for loop with credits example.

--- C Class/programs/practice1.c
+++ C Class/programs/practice1.c
@@ -2,6 +2,8 @@
 
 
 // requirement is print Hello for 5 times.
+// requirement is, If you wish you loose a credit, Wish untill you don't have any credit left.
+// Your current credits are 50;
 
 // 1. Initialization
 // 2. Condition Checking
@@ -18,6 +20,23 @@ int main()
         printf(" current value of i is %d\n", i);
         printf("Hello!\n");
     }
+
+    int credits = 50;
+    for(int i=1; i<=credits; i++)
+    {
+        printf(" current value of i is %d\n", i);
+        printf("Wish!\n");
+    }
+
+    for(int i= credits; i>=1; i--)
+    {
+        printf(" current value of i is %d\n", i);
+        printf("Wish!\n");
+    }
+
+
     
     return 0;  // return 0 to indicate successful execution
 }
+
+
